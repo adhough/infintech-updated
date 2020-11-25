23650a67907c7cbfe4b5ea07f31e7f21a0689c56
adhough <33616464+adhough@users.noreply.github.com>
2020-11-25 08:41:05 +0000

    76th amend

--- src/components/FXRisk.js
+++ src/components/FXRisk.js
@@ -17,7 +17,7 @@ function FXRiskSection() {
 <br />
 <img src={FXModel} alt="fx"/>
 <br />
-<p>This model calculates the one day Value at Risk (VaR) for a portfolio of assets held in currencies such as Australian Dollars (AUD), Canadian Dollars (CAD), Swiss Francs (CHF), Euros (EUR), Japanese Yen (JPY), New Zealand Dollars (NZD) and US Dollars (USD). This VaR model estimates the most that a portfolio can lose in one day, due to changes in FX rates given either 95% or 99% confidence intervals . This model is measuring the FX risk and not other risk factors such as equity, interest or credit risk. Valuations are in British Pound (GBP).  This website was developed using Python, Flask, Numpy, Pandas and deployed using Heroku.
+<p>This model calculates the one day Value at Risk (VaR) for a portfolio of assets held in a range of currencies. This VaR model estimates the most that a portfolio can lose in one day, due to changes in FX rates given either 95% or 99% confidence intervals .  Valuations are in British Pound (GBP).  This website was developed using Python, Flask, Numpy, Pandas and deployed using Heroku.
 </p>
 <br />
 <a href="https://fxriskmodel.herokuapp.com/" target="_blank" rel="noreferrer">https://fxriskmodel.herokuapp.com/</a>
